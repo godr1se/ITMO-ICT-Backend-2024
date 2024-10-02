deee461ff06f4aef18927c7506c7f999a3ccdfc1
godr1se <90646570+godr1se@users.noreply.github.com>
2024-10-02 15:19:01 +0300

    Delete incorrect labs

--- labs/K33402/lab3,lab4/main/src/routes/history/index.ts
+++
@@ -1,9 +0,0 @@
-import HistoryControll from "../../controllers/history";
-import Express from 'express';
-
-const rout: Express.Router = Express.Router();
-const history = new HistoryControll()
-
-rout.get('/:id', history.get);
-
-export default rout
